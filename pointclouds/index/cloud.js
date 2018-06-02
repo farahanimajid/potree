331ff38f2ef0a5cfd7298a232ccf1b9d8d7646f5
farahanimajid <hosseinm@oregonstate.edu>
2018-06-02 14:36:43 -0700

    3

---
+++ pointclouds/index/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 4793930,
+    "boundingBox": {
+        "lx": -0.8149999999999995,
+        "ly": -2.9692,
+        "lz": -6.4605,
+        "ux": 6.139900000000002,
+        "uy": 3.9857000000000015,
+        "uz": 0.4944000000000015
+    },
+    "tightBoundingBox": {
+        "lx": -0.8149999999999995,
+        "ly": -2.9692,
+        "lz": -6.4605,
+        "ux": 4.347300000000001,
+        "uy": 3.9857000000000015,
+        "uz": -1.2637
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 0.060231201350688937,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
